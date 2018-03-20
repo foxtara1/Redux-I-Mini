97b40a2edbea7e322f83176d0516bce318062700
Abraham Andujo <foxtara00@gmail.com>
2018-03-20 15:41:35 -0700

    complete

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,9 @@
+
+import { combineReducers } from 'redux';
+import todosReducers from './todosReducers';
+
+const rootReducer = combineReducers({
+    todos: todosReducers
+});
+
+export default rootReducer;
